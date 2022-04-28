87ec998f5b886a642b676ef05dd1871faf0f1831
Elianne <a20185361@pucp.edu.pe>
2022-04-27 21:25:45 -0500

    corr

--- src/main/java/edu/pucp/gtics/lab4_gtics_20221/controller/JuegosController.java
+++ src/main/java/edu/pucp/gtics/lab4_gtics_20221/controller/JuegosController.java
@@ -45,22 +45,46 @@ public class JuegosController {
 
     @GetMapping("/new")
     public String nuevoJuegos(@ModelAttribute("juego") Juegos juego, Model model) {
-        model.addAttribute("listaGenero",generosRepository.findAll());
-        model.addAttribute("listaPlataform",plataformasRepository.findAll());
-        model.addAttribute("listaDistribuidoras",distribuidorasRepository.findAll());
+        model.addAttribute("listaGenero",getListaGenero());
+        model.addAttribute("listaPlataform",getListaPlataform());
+        model.addAttribute("listaDistribuidoras",getListaDistribuidoras());
         return "juegos/editarFrm";
+    }
+    public List<Generos> getListaGenero() {
+        List<Generos> listaGenero = generosRepository.findAll();
+        Generos e = new Generos();
+        e.setIdgenero(0);
+        e.setNombre("--Seleccione--");
+        listaGenero.add(0, e);
+        return listaGenero;
+    }
+    public List<Plataformas> getListaPlataform() {
+        List<Plataformas> listaPlataform = plataformasRepository.findAll();
+        Plataformas e = new Plataformas();
+        e.setIdplataforma(0);
+        e.setNombre("--Seleccione--");
+        listaPlataform.add(0, e);
+        return listaPlataform;
+    }
+    public List<Distribuidoras> getListaDistribuidoras() {
+        List<Distribuidoras> listaDistribuidoras = distribuidorasRepository.findAll();
+        Distribuidoras e = new Distribuidoras();
+        e.setIddistribuidora(0);
+        e.setNombre("--Seleccione--");
+        listaDistribuidoras.add(0, e);
+        return listaDistribuidoras;
     }
 
     @PostMapping("/save")
     public String guardarJuego(@ModelAttribute("juego") @Valid Juegos juego, BindingResult bindingResult,
                                   RedirectAttributes attr, Model model) {
         if(bindingResult.hasErrors()){
-            model.addAttribute("listaGenero",generosRepository.findAll());
-            model.addAttribute("listaPlataform",plataformasRepository.findAll());
-            model.addAttribute("listaDistribuidoras",distribuidorasRepository.findAll());
+            model.addAttribute("listaGenero",getListaGenero());
+            model.addAttribute("listaPlataform",getListaPlataform());
+            model.addAttribute("listaDistribuidoras",getListaDistribuidoras());
             return "juegos/editarFrm";
         }else {
-            if (juego.getId() == 0) {
+            if (juego.getId() == 0 || juego.getId() > juegosRepository.count() ) {
                 attr.addFlashAttribute("msg", "Juego creado exitosamente");
                 juegosRepository.save(juego);
                 return "redirect:/juegos/lista";
@@ -74,14 +98,14 @@ public class JuegosController {
     }
     @GetMapping("/edit")
     public String editarJuego(@ModelAttribute("juego") Juegos juegos, Model model,
-                                 @RequestParam("id") int id) {
+                                 @RequestParam(value="id", required = false, defaultValue = "1") int id) {
         Optional<Juegos> juegosOptional = juegosRepository.findById(id);
         if(juegosOptional.isPresent()){
             Juegos juego = juegosOptional.get();
             model.addAttribute("juego",juego);
-            model.addAttribute("listaGenero",generosRepository.findAll());
-            model.addAttribute("listaPlataform",plataformasRepository.findAll());
-            model.addAttribute("listaDistribuidoras",distribuidorasRepository.findAll());
+            model.addAttribute("listaGenero",getListaGenero());
+            model.addAttribute("listaPlataform",getListaPlataform());
+            model.addAttribute("listaDistribuidoras",getListaDistribuidoras());
             return "juegos/editarFrm";
         }
         return "redirect:/juegos/lista";
